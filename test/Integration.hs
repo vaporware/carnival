81c8e061ef595b05f8669bac52bd5010febe01a0
patrick brisbin <pbrisbin@gmail.com>
2015-04-27 20:26:16 -0400

    Fixup integration spec

--- test/Integration.hs
+++ test/Integration.hs
@@ -1,12 +1,6 @@
 module Main where
 
-import TestImport
-import Test.Hspec.WebDriver hiding (shouldReturn)
-import Test.WebDriver.Commands.Wait
-
-import Control.Monad.Logger
-import Database.Persist.Postgresql
-import Database.Persist.Sql
+import IntegrationImport
 
 main :: IO ()
 main = hspec spec
@@ -17,11 +11,12 @@ spec = do
         pool <- createDBPool
         runDBWithPool pool $ createUser "1"
 
-    describe "" $ session "" $ using Firefox $ do
+    describe "logging in to create a comment" $ session "" $ using Firefox $ do
         it "loads carnival" $ runWD $ do
             openPage "http://localhost:3000"
-            click =<< waitForElem (ByCSS ".carnival-comment-indicator")
-            click =<< waitForElem (ByCSS ".carnival-create")
+
+            clickOn $ ByCSS ".carnival-comment-indicator"
+            clickOn $ ByCSS ".carnival-create"
 
             inLoginWindow $
                 submit =<< fillIn (ByName "ident") (userIdent $ entityVal user)
@@ -29,36 +24,8 @@ spec = do
             submit =<< fillIn (ByCSS ".carnival-body") "a comment"
 
             openPage "http://localhost:3000"
-            click =<< waitForElem (ByCSS ".carnival-comment-indicator")
-            click =<< waitForElem (ByCSS ".carnival-create")
+            clickOn $ ByCSS ".carnival-comment-indicator"
+            clickOn $ ByCSS ".carnival-create"
 
-            (getText =<< waitForElem (ByCSS ".carnival-comment .carnival-body"))
+            waitForText (ByCSS ".carnival-comment .carnival-body")
                 `shouldReturn` "a comment"
-
-inLoginWindow f = do
-    cw <- getCurrentWindow
-    focusWindow =<< return . unsafeHead . filter (/= cw) =<< windows
-    f
-    focusWindow cw
-
-login ident = do
-    openPage "http://localhost:3000/auth/login"
-    submit =<< fillIn (ByName "ident") "1"
-
-fillIn x value = do
-    input <- findElem x
-    sendKeys value input
-    return input
-
-waitForElem = waitUntil 50000 . findElem
-
-createDBPool :: IO ConnectionPool
-createDBPool = do
-    settings <- getAppSettings
-    pool <- runStderrLoggingT $ createPostgresqlPool
-        (pgConnStr $ appDatabaseConf settings)
-        (pgPoolSize $ appDatabaseConf settings)
-
-    wipeDBPool pool
-
-    return pool
---
+++ test/IntegrationImport.hs
@@ -0,0 +1,53 @@
+module IntegrationImport
+    ( createDBPool
+    , clickOn
+    , fillIn
+    , inLoginWindow
+    , waitForElem
+    , waitForText
+    , module X
+    ) where
+
+import ClassyPrelude as X hiding (Element)
+import TestImport as X (getAppSettings, wipeDBPool)
+import Test.Hspec.WebDriver as X hiding (shouldBe, shouldReturn)
+
+import Settings
+import Test.WebDriver.Class
+import Test.WebDriver.Commands.Wait
+
+import Control.Monad.Logger
+import Database.Persist.Postgresql
+
+inLoginWindow :: WebDriver m => m a -> m ()
+inLoginWindow f = do
+    cw <- getCurrentWindow
+    focusWindow =<< return . unsafeHead . filter (/= cw) =<< windows
+    void $ f
+    focusWindow cw
+
+clickOn :: WebDriver m => Selector -> m ()
+clickOn = click <=< waitForElem
+
+fillIn :: WebDriver m => Selector -> Text -> m Element
+fillIn x value = do
+    input <- findElem x
+    sendKeys value input
+    return input
+
+waitForText :: WebDriver m => Selector -> m Text
+waitForText = getText <=< waitForElem
+
+waitForElem :: WebDriver m => Selector -> m Element
+waitForElem = waitUntil 50000 . findElem
+
+createDBPool :: IO ConnectionPool
+createDBPool = do
+    settings <- getAppSettings
+    pool <- runStderrLoggingT $ createPostgresqlPool
+        (pgConnStr $ appDatabaseConf settings)
+        (pgPoolSize $ appDatabaseConf settings)
+
+    wipeDBPool pool
+
+    return pool
